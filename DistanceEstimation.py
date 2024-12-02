150a66a8b584b89cbc83cafb4f6595aa112cb146
Private_Brindz <88164645+Brindhamanick@users.noreply.github.com>
2024-12-03 01:49:31 +0530

    Update DistanceEstimation.py

--- DistanceEstimation.py
+++ DistanceEstimation.py
@@ -27,33 +27,14 @@ BLACK = (0, 0, 0)
 # Defining fonts
 FONTS = cv.FONT_HERSHEY_PLAIN
 
-
-# @st.cache_resource
-def load_model(model_path, device):
-    core = Core()
-    return core.compile_model(model_path, device)
-
-# @st.cache_resource
-def load_seg_model(model_path):
+@st.cache_resource
+def load_model(model_path):
     # Load and return the YOLO model
-    return YOLO(model_path)
+    return YOLO(model_path, 'conf=0.40')
 
-
-# Ensure the correct paths to the .xml and .bin files
-model_path = "yolov8x_openvino_model/yolov8c.xml"
-device = "CPU"
-# Load the model
-try:
-    model = load_model(model_path, device)
-    st.write("Optimized Openvino Yolov8c Models loaded successfully!")
-          
-except Exception as e:
-    print(f"Error loading model: {e}")
-
-
-# # Load the YOLOv8 model
-# model_select = "yolov8xcdark.pt"
-# model = YOLO(model_select,'conf=0.45')  # You can replace it with 'yolov8-tiny.pt' if you want a smaller version
+# Load the YOLOv8 model
+model_select = "yolov8xcdark.pt"
+model = load_model(model_select) # You can replace it with 'yolov8-tiny.pt' if you want a smaller version
 
 # Get class names from the YOLO model
 class_names = model.names
